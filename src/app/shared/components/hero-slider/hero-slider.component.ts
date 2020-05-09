dc5ca11f3734d8921871d79672b1db3c327add4b
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2020-05-09 13:07:41 +0100

    Added Why-Us section

--- src/app/shared/components/hero-slider/hero-slider.component.ts
+++ src/app/shared/components/hero-slider/hero-slider.component.ts
@@ -8,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HeroSliderComponent implements OnInit {
 
   slides = [
-    {img: "assets/images/eugene-lim-304646.jpg"},
-    {img: "assets/images/Layer-171_03.jpg"},
-    {img: "assets/images/oslider1-s3_03.jpg"},
+    {
+      img: "assets/images/eugene-lim-304646.jpg",
+      content: 'We help making change happen'
+    },
+    {
+      img: "assets/images/Layer-171_03.jpg",
+      content: 'We help making change happen'
+    },
+    {
+      img: "assets/images/oslider1-s3_03.jpg",
+      content: 'Stay with us You gonna love it'
+    },
     {img: "assets/images/s1-slide-1.jpg"}
   ];
 
--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSliderComponent } from '../components/hero-slider/hero-slider.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { WhyUsComponent } from '../components/why-us/why-us.component';
 
 
 @NgModule({
   declarations: [
-    HeroSliderComponent
+    HeroSliderComponent,
+    WhyUsComponent
   ],
   imports: [
     CommonModule,
     SlickCarouselModule
   ],
   exports: [
-    HeroSliderComponent
+    HeroSliderComponent,
+    WhyUsComponent
   ]
 })
 export class SharedModule { }
