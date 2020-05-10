aa420db8e78b19d743b581a5324aa9a6e9455218
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2020-05-10 16:31:38 +0100

    Added slider content

--- src/app/core/layouts/hero-slider/hero-slider.component.ts
+++ src/app/core/layouts/hero-slider/hero-slider.component.ts
@@ -6,31 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-slider.component.scss']
 })
 export class HeroSliderComponent implements OnInit {
-
-  slides = [
-    {
-      img: "assets/images/image1-min.jpg",
-      content: 'We help making change happen'
-    },
-    {
-      img: "assets/images/image2-min.jpg",
-      content: 'We help making change happen'
-    },
-    {
-      img: "assets/images/image3-min.jpg",
-      content: 'Stay with us You gonna love it'
-    },
-    {img: "assets/images/image4-min.jpg"}
-  ];
-
   slideConfig = {
+    "autoplay": true,
+    "autoplaySpeed": 2000,
     "slidesToShow": 1,
-    "slidesToScroll": 1
+    "slidesToScroll": 1,
+    "dots": true,
+    "fade": true,
+    "cssEase": 'linear',
+    "arrows": true
   };
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
