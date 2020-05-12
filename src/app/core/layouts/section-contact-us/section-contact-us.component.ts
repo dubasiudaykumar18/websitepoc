9f11318b4160be3325910a80e0d0879f58180a72
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2020-05-12 22:32:58 +0100

    Added section-contact-us

---
+++ src/app/core/layouts/section-contact-us/section-contact-us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-contact-us',
+  templateUrl: './section-contact-us.component.html',
+  styleUrls: ['./section-contact-us.component.scss']
+})
+export class SectionContactUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
