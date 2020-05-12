49b4a99a7353c234888afbb47a48b3f42d27fd80
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2020-05-12 21:30:22 +0100

    Added section-about-us

--- src/app/pages/about-us/about-us.module.ts
+++ src/app/pages/about-us/about-us.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AboutUsComponent } from './about-us.component';
 import { RouterModule } from '@angular/router';
 import { aboutUsRouters } from './about-us.routering';
+import { SectionAboutUsComponent } from 'src/app/core/layouts/section-about-us/section-about-us.component';
 
 
 
 @NgModule({
   declarations: [
-    AboutUsComponent
+    AboutUsComponent,
+    SectionAboutUsComponent
   ],
   imports: [
     CommonModule,
