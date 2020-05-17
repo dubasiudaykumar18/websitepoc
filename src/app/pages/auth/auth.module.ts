4a3409dd9ec1986c95b3bd8d0025db8aa9a994cd
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2020-05-17 19:38:51 +0100

    Added unit-test for sign-in-workspace

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SignInWorkspaceComponent } from './sign-in-workspace/sign-in-workspace.component';
 import { RouterModule } from '@angular/router';
 import { authRouters } from './auth.routering';
@@ -16,6 +17,7 @@ import { GetStartedComponent } from './get-started/get-started.component';
   ],
   imports: [
     RouterModule.forChild(authRouters),
+    ReactiveFormsModule,
     CommonModule
   ]
 })
