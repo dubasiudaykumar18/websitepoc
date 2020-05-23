56f40a3bd6ed9e7decba1e4ca3a835cda9c25d11
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2020-05-23 10:47:26 +0100

    Add url constants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-
+import { UrlConstants } from '../app/core/services/url-constants';
 
 const routes: Routes = [
-  {  path: '', redirectTo: 'home', pathMatch: 'full' },
-  {  path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  {  path: 'about-us', loadChildren: () => import('./pages/about-us/about-us.module').then(m => m.AboutUsModule) },
-  {  path: 'contact-us', loadChildren: () => import('./pages/contact-us/contact-us.module').then(m => m.ContactUsModule) },
-  {  path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  {  path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
-  {  path: '404',  component: PageNotFoundComponent },
+  {  path: UrlConstants.HOME, redirectTo: 'home', pathMatch: 'full' },
+  {  path: UrlConstants.HOME, loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  {  path: UrlConstants.ABOUT_US, loadChildren: () => import('./pages/about-us/about-us.module').then(m => m.AboutUsModule) },
+  {  path: UrlConstants.CONTACT_US, loadChildren: () => import('./pages/contact-us/contact-us.module').then(m => m.ContactUsModule) },
+  {  path: UrlConstants.DASHBOARD, loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  {  path: UrlConstants.AUTH, loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  {  path: UrlConstants.PAGE_NOT_FOUND,  component: PageNotFoundComponent },
   {  path: '**', component: PageNotFoundComponent }
 ];
 
---
+++ src/app/core/services/url-constants.ts
@@ -0,0 +1,12 @@
+/**
+ * Constants for urls.
+ */
+
+ export const UrlConstants = {
+    HOME: 'home',
+    ABOUT_US: 'about-us',
+    CONTACT_US: 'contact-us',
+    DASHBOARD: 'dashboard',
+    AUTH: 'auth',
+    PAGE_NOT_FOUND: '404' 
+ }
